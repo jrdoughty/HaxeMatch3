365441f35804e833c5eecb74d99957417a04e4f2
John Doughty <jrdoughty13@gmail.com>
2015-09-10 20:15:23 -0500

    Created neighbor node system and created swap

--- src/Board.hx
+++ src/Board.hx
@@ -1,12 +1,12 @@
 using Std;
-import luxe.Sprite;
 import luxe.Input;
 import luxe.Vector;
 import luxe.Color;
 
 class Board 
 {
-    public var gems: Array<Sprite> = new Array();
+    public var gems: Array<Gem> = new Array();
+    public var selectedGems: Array<Gem> = new Array();
     public function new()
     {
         var i: Int;
@@ -20,17 +20,59 @@ class Board
             numImage = Math.floor(Math.random() * Main.NUMGEMIMAGES);
             gems.push(new Gem(numImage, x, y, "down"));
         }
+        for (i in 0...gems.length) 
+        {
+            buildNeighbors(gems[i]);
+        }
     }
 
     public function onMouseUp(pos:Vector)
     {
+        var i: Int = 0;
         var x: Float = (pos.x) - pos.x % Main.GEMIMAGESIZE;
         var y: Float = (pos.y) - pos.y % Main.GEMIMAGESIZE;
         x = x / Main.GEMIMAGESIZE;
         y = y / Main.GEMIMAGESIZE;
-        var indexFloat:Float = x + y * Main.BOARDWIDTH;
-        var index:Int = Math.floor(indexFloat);
-        gems[index].color = new Color().rgb(0xf94b04);
+        
+        for (i in 0...gems.length) {
+            if(gems[i].x == x && gems[i].y == y)
+            {
+                gems[i].color = new Color().rgb(0xdddddd);
+                selectedGems.push(gems[i]);
+                break;
+            }
+        }
+
+        if(selectedGems.length == 2)
+        {
+            var oldPosofFirst = new Vector(selectedGems[0].x,selectedGems[0].y);
+            selectedGems[0].swap(selectedGems[1].x,selectedGems[1].y);
+            selectedGems[1].swap(cast(oldPosofFirst.x, Int),cast(oldPosofFirst.y, Int));
+            buildNeighbors(selectedGems[0]);
+            buildNeighbors(selectedGems[1]);
+            selectedGems[0].color = new Color().rgb(0xffffff);
+            selectedGems[1].color = new Color().rgb(0xffffff);
+            selectedGems = new Array();
+        }
+    }
+
+    public function buildNeighbors(gem:Gem)
+    {
+        for (i in 0...gems.length) 
+        {
+            gem.neighborNodes = new Array();
+            if(gem.x >= gems[i].x - 1 && gem.x <= gems[i].x + 1 &&
+               gem.y >= gems[i].y - 1 && gem.y <= gems[i].y + 1 &&
+               (gem.x == gems[i].x && gem.y == gems[i].y) == false)
+            {
+                gem.neighborNodes.push(gems[i]);
+            }
+        }
+    }
+
+    public function isValidMove()
+    {
+        selectedGems[0];
     }
 
 }
--- src/Gem.hx
+++ src/Gem.hx
@@ -1,5 +1,5 @@
 import luxe.Sprite;
-import phoenix.Vector;
+import luxe.Vector;
 import luxe.Color;
 
 class Gem extends Sprite 
@@ -7,8 +7,10 @@ class Gem extends Sprite
     public var numImage: Int = 0;
     public var direction: String = "";
     public var sprite: Sprite;
+    public var neighborNodes:Array<Gem>;
     public var x: Int;
     public var y: Int;
+
     public function new (numImage: Int, xCord: Int, yCord: Int, direction: String)
     {
         super({
@@ -24,4 +26,12 @@ class Gem extends Sprite
         this.direction = direction;
         
     }
+
+    public function swap(x:Int, y:Int)
+    {
+        pos = new Vector(x * Main.GEMIMAGESIZE + Main.GEMIMAGESIZE / 2, y * Main.GEMIMAGESIZE + Main.GEMIMAGESIZE / 2);
+        this.x = x;
+        this.y = y;
+    }
+
 }
